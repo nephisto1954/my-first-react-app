67c231a3b429122b5aa3d4bcc53016f46f166ba6
tanguy chennaux <tanguy.chennaux@gmail.com>
2018-03-21 16:00:50 +0000

    more info

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+
+  }
+
   render() {
     return (
       <div className="App">
         <h1> I am a React App </h1>
         <p> This is really working!</p>
+        <button>Switch Name</button>
         <Person name="Max" age="28"/>
         <Person name="Manu" age="29">My Hobby is diving</Person>
         <Person name="Stephany" age="26"/>
